0cd4ff9dd1e87e0c55bb6043346df2752dcd030c
lcol3117 <59921084+lcol3117@users.noreply.github.com>
2020-09-10 11:25:32 -0400

    Update core.clj

--- holes/core.clj
+++ holes/core.clj
@@ -1,1 +1,22 @@
 (ns holes.core)
+
+(defn test-core []
+  (do
+    (def my-hmap (hmap {
+                         :name      "Landon"
+                         :languages [
+                                      :clojure
+                                      :python3
+                                      :rust
+                                      :julia
+                                      :java
+                                      :elixir
+                                      :c++
+                                      :typescript
+                                      :kotlin]
+                         :emotion   :happy}
+                       [
+                         :age
+                         :favorite-number]))
+    (assert (= ))
+    ))
